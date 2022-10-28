b7a7f4ef8c47e6171e2c5d52ddf9de8b9de5adfb
LizaAkishina <lizaakishina@gmail.com>
2022-10-28 15:16:03 +0300

    fix: move dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
